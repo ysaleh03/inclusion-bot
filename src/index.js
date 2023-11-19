6438b4fcb8e17e5e2a6e07a9c85deea156bf659d
Youssef Saleh <ymseam@hotmail.com>
2023-11-19 11:30:53 -0800

    bot now replies to us with our own messages :)

--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+client.on(Events.MessageCreate, c => {
+	if (c.author.bot) return;
+
+	console.log(`${c.author}: ${c.content}`);
+	client.channels.cache.get(c.channelId).send(`${c.author}: ${c.content}`);
+})
 
 // Log in to Discord with your client's token
 client.login(token);
