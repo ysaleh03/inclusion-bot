9521c38c9c90840202962934733e0367ce4b61ce
Ke Ren <rkglimmer16@gmail.com>
2023-11-19 10:58:51 -0800

    Configure basic login entry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const { DISCORD_TOKEN, COHERE_API_KEY } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(DISCORD_TOKEN);
